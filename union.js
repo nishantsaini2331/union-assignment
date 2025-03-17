b82d655ab14c1e5ca8c0f539bd815b4a9057762c
Nishant Saini <ns0109375@gmail.com>
2025-03-17 20:25:31 +0530

    handle nested objects

--- union.js
+++ union.js
@@ -1,13 +1,50 @@
-function union(arr1, arr2) {
-  let newArr = [];
-  let combineArr = [...arr1, ...arr2];
-  for (let i = 0; i < combineArr.length; i++) {
-    if (!newArr.includes(combineArr[i])) {
-      newArr.push(combineArr[i]);
-    }
+function deepEqual(obj1, obj2) {
+  if (obj1 === obj2) return true;
+
+  if (
+    typeof obj1 !== "object" ||
+    typeof obj2 !== "object" ||
+    obj1 === null ||
+    obj2 === null
+  ) {
+    return false;
   }
 
-  return newArr;
+  const keys1 = Object.keys(obj1);
+  const keys2 = Object.keys(obj2);
+
+  if (keys1.length !== keys2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < keys1.length; i++) {
+    const key = keys1[i];
+    if (!deepEqual(obj1[key], obj2[key])) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function union(arr1, arr2) {
+  const result = [];
+
+  [...arr1, ...arr2].forEach((item) => {
+    let isDuplicate = false;
+
+    for (let i = 0; i < result.length; i++) {
+      if (deepEqual(result[i], item)) {
+        isDuplicate = true;
+        break;
+      }
+    }
+
+    if (!isDuplicate) {
+      result.push(item);
+    }
+  });
+
+  return result;
 }
 
 export default union;
